41121400b19d10bfeeea730fc9376c8b896e29e3
ilovetdi <35616154+ilovetdi@users.noreply.github.com>
2026-04-29 22:37:07 +0200

    fix21

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import React,{useState} from "react";
 import Login from "./Login";
 import Map from "./Map";
+import Admin from "./Admin";
 
 export default function App(){
-  const [token,setToken]=useState(localStorage.getItem("token"));
+ const [token,setToken]=useState(localStorage.getItem("token"));
 
-  try {
-    if(!token) return <Login setToken={setToken}/>;
-    return <Map token={token}/>;
-  } catch(e){
-    console.error("APP CRASH:", e);
-    return <div style={{padding:20}}>Hiba történt</div>;
-  }
+ if(!token) return <Login setToken={setToken}/>;
+
+ if(window.location.pathname === "/admin"){
+   return <Admin token={token} />;
+ }
+
+ return <Map token={token}/>;
 }
